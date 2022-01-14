0f814b77c9bed6fff9e4f0e491fbf34bc7f243c4
João Marcos <joao@agenciagh.com.br>
2022-01-13 23:38:56 -0300

    stack navigator

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 
-export default () => (
+export default ({ navigation }) => (
     <View>
         <Text>Contato</Text>
+        <Text
+            onPress={() => navigation.navigate('Information')}
+        >
+            Information...
+        </Text>
     </View>
 )
