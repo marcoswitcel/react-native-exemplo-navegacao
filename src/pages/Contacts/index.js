ea64c1bf61a9f698f68e62186be2a17378593cf9
João Marcos <joao@agenciagh.com.br>
2022-01-13 23:56:35 -0300

    stack navigation com dados sendo passados

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -3,12 +3,32 @@ import { Text, View } from 'react-native';
 
 
 export default ({ navigation }) => (
-    <View>
-        <Text>Contato</Text>
-        <Text
-            onPress={() => navigation.navigate('Information')}
-        >
-            Information...
-        </Text>
+    <View style={{marginTop: 60}}>
+        <View>
+            <Text>Nome: Teste Do Teste</Text>
+            <Text>Telefon: (99) 99999-9999</Text>
+            <Text onPress={() => navigation.navigate('Information', {
+                nome: 'Teste Do Teste',
+                telefone: '(99) 99999-9999',
+                endereco: 'Rua das Flores',
+                numero: '165',
+                profissao: 'TI',
+            })}>
+                Information...
+            </Text>
+        </View>
+        <View style={{marginTop: 10}}>
+            <Text>Nome: Teste Do Testestado</Text>
+            <Text>Telefon: (99) 99999-9999</Text>
+            <Text onPress={() => navigation.navigate('Information', {
+                nome: 'Teste Do Testestado',
+                telefone: '(99) 99999-9999',
+                endereco: 'Rua das Flores',
+                numero: '165',
+                profissao: 'TI',
+            })}>
+                Information...
+            </Text>
+        </View>
     </View>
 )
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default () => (
-    <View>
-        <Text>Informações</Text>
-    </View>
-)
+export default ({ route }) => {
+    const {nome, telefone, endereco, numero, profissao } = route.params;
+
+    return (
+        <View style={{marginTop: 60}}>
+            <Text>nome: { nome }</Text>
+            <Text>telefone: { telefone }</Text>
+            <Text>endereco: { endereco }</Text>
+            <Text>numero: { numero }</Text>
+            <Text>profissao: { profissao }</Text>
+        </View>
+    )
+}
