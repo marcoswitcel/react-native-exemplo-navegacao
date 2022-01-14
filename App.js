beb88cc90976c7a00f706ce13ffd37e2e6dff7f2
João Marcos <joao@agenciagh.com.br>
2022-01-14 00:21:10 -0300

    app funcionando e readme iniciado

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Contacts from './src/pages/Contacts';
 import Information from './src/pages/Information';
+import AppContacts from './src/pages/AppContacts';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="AppContacts" component={AppContacts} />
+      <Tab.Screen name="Contacts" component={Contacts} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Contacts" component={Contacts} />
+        <Stack.Screen name="AppContacts" component={Tabs} />
         <Stack.Screen name="Information" component={Information} />
       </Stack.Navigator>
     </NavigationContainer>
