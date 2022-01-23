7f466163185a6e497baaafa607d0f1473a33b667
João Marcos <joao@agenciagh.com.br>
2022-01-23 00:54:34 -0300

    a lista agora tem o próprio estado

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Text, View, TouchableOpacity } from 'react-native';
+import { ContactRepository } from '../repository/ContactRepository';
 import style from './style';
 
 
-export default ({ route, navigation }) => (
-    <View style={style.listContainer}>
-        <FlatList
-            data={route.params.contactsListRef.current}
-            renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => navigation.navigate('Information', item)}>
-                    <View style={style.listItem}>
-                        <Text>Nome: {item.nome}</Text>
-                        <Text>Telefone: {item.telefone}</Text>
-                        <Text>Ver mais</Text>
-                    </View>
-                </TouchableOpacity>
-            )}
-            keyExtractor={(item) => item.id}
-        />
-    </View>
-);
+export default ({ navigation }) => {
+    const [ contactsList, setContactsList ] = useState([]);
+
+    useEffect(() => {
+        ContactRepository.getAll().then(setContactsList)
+    }, []);
+
+    return (
+        <View style={style.listContainer}>
+            <FlatList
+                data={contactsList}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Information', item)}>
+                        <View style={style.listItem}>
+                            <Text>Nome: {item.nome}</Text>
+                            <Text>Telefone: {item.telefone}</Text>
+                            <Text>Ver mais</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+    );
+};
