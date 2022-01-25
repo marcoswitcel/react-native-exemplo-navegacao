3e84b0d5ce8adfa46afb50d225b22f7a34daed5e
João Marcos <joao.marcos.witcel@gmail.com>
2022-01-24 22:53:52 -0300

    padronização de tabsize

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,13 +3,13 @@ import { Text, View, Button } from 'react-native';
 import style from './style';
 
 export const Dashboard = ({ navigation }) => (
-	<View style={style.container}>
-		<Text style={style.text}>Gerenciador de contatos</Text>
-		<Button
-			title='Cadastrar novo contato'
-			onPress={() => navigation.navigate('AddContact')}
-		/>
-	</View>
+  <View style={style.container}>
+    <Text style={style.text}>Gerenciador de contatos</Text>
+  <Button
+    title='Cadastrar novo contato'
+    onPress={() => navigation.navigate('AddContact')}
+  />
+  </View>
 );
 
 export default Dashboard;
